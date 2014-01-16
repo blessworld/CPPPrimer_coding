19f3b26402364b044464f1f4e01fd9357780f2fa
blessworld <thedarkknight@BroadCast-Wifi.local>
2014-01-16 14:27:58 +0800

    v1 书上第二章….

---
+++ -private-CPP_Primer_type_by_myself/CPPPrimer/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CPPPrimer
+//
+//  Created by TheDarkKnight on 14-1-14.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "\nCC\toption\tfile.[cC]\n";
+    std::cout << "a really really long string "
+    << "that split two lines" << std::endl;
+    std::cout << "M" << "\115" << "\x4d" << std::endl;
+    unsigned long long a = -1ull + 0 ;
+    std::cout << a << std::endl;
+    return 0;
+}
+
